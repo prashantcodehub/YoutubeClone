d7de1b802ac9fcf0b54c6f559b3f826914b0e079
prashantcodehub <113547581+prashantcodehub@users.noreply.github.com>
2023-07-03 01:18:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector('.video-container');
 
- let api_key = "AIzaSyAzyxV87JPsutRo8rhxVPBmie7rMcJzHAc"; //AIzaSyAzyxV87JPsutRo8rhxVPBmie7rMcJzHAc
+ let api_key = "API key here"; 
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
